324743de38ed1648258085fbd86ab678e55f2117
Hassain Saeed <hassain.saeed@gmail.com>
2022-11-04 23:14:04 -0700

    Remove gatsby-source-filesystem plugin from gastby config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,4 @@
 const path = require(`path`)
-// const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
@@ -13,20 +12,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     },
   })
 }
-
-// exports.onCreateNode = ({ node, actions, getNode }) => {
-//   const { createNodeField } = actions
-
-//   if (node.internal.type === `MarkdownRemark`) {
-//     const value = createFilePath({ node, getNode })
-
-//     createNodeField({
-//       name: `slug`,
-//       node,
-//       value,
-//     })
-//   }
-// }
 
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
